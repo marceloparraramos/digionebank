fe2d2196b440fc3af2a4834d6b04f4096e3e6b8a
Marcelo <marceloparraramos@gmail.com>
2021-07-13 00:10:40 -0300

    Commit inicial

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Marcelo"
+    var cpf: String = "123.123.123-12"
+}
+
+fun main() {
+    val marcelo = Pessoa()
+    println(marcelo.nome)
+    println(marcelo.cpf)
+}
