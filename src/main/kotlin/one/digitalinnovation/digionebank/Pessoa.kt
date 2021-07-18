1559c55ae8f85176cf2b17cd9f041dff620cc346
Marcelo <marceloparraramos@gmail.com>
2021-07-18 11:57:28 -0300

    Adicionada função infoPessoa à classe Pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,15 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Marcelo"
-
     var cpf: String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun infoPessoa() = "$nome e $cpf"
 }
 
 fun main() {
     val marcelo = Pessoa()
 
-    println(marcelo)
-    println(marcelo.nome)
-    println(marcelo.cpf)
+    println(marcelo.infoPessoa())
 }
