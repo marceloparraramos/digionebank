e04c806491d7f45063e4d7a557bac4446a5ba803
Marcelo <marceloparraramos@gmail.com>
2021-07-18 11:48:37 -0300

    Aplicado modificador private para propriedade cpf da classe Pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,11 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Marcelo"
+
     var cpf: String = "123.123.123-12"
+    private set
 }
 
 fun main() {
     val marcelo = Pessoa()
+
+    println(marcelo)
     println(marcelo.nome)
     println(marcelo.cpf)
 }
